3b3c74e8cf92d5dc6290e3f6aa92110ff6fefd65
Thiago <lemes_vilarinho@yahoo.com.br>
2021-11-05 10:31:51 -0300

    Criado projeto agenda e a tabela eventos

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models.fields import DateTimeField
+from django.contrib.auth.models import User # importando o models User do django
+
+# Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='DATA DO EVENTO')
+    data_criacao = models.DateTimeField(verbose_name='DATA DE CRIAÇÃO', auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) # chave estrangeira
+
+    # Especificando o nome da tabela
+    class Meta:
+        db_table = 'evento'
+
+    # Altera como será mostrado no Painel
+    # def __str__(self):
+    #     return self.titulo
